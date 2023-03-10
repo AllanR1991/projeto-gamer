0d852e0933d216021536de88a572c6b50e353377
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-03-09 22:15:03 -0300

    Feito o header mobile e com ações dos botões.

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+function exibeMenuNav(){
+    document.getElementById("navMenuEsquerda").classList.toggle("mostrarMenuEsquerda");
+}
+
+function exibeMenuUser(){
+    document.getElementById("navMenuDireita").classList.toggle("mostrarMenuDireita");
+}
